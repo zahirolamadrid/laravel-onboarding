43a8acde723d66237f116cd14cdca224cf4ea6d5
Diana <dlgarces9218@gmail.com>
2023-12-13 14:19:31 -0600

    Add spaces between fields

--- resources/views/leads.blade.php
+++ resources/views/leads.blade.php
@@ -1,22 +1,27 @@
 <x-layout>
     <form method="POST" action="/leads">
         @csrf
+        <br>
         <div>
             <label for="name">Primer nombre:</label>
             <input type="text" id="name" name="name" required>
         </div>
+        <br>
         <div>
             <label for="second-name">Segundo nombre:</label>
             <input type="text" id="second-name" name="second-name">
         </div>
+        <br>
         <div>
             <label for="last-name">Primer apellido:</label>
             <input type="text" id="last-name" name="last-name" required>
         </div>
+        <br>
         <div>
             <label for="second-last-name">Segundo apellido</label>
             <input type="text" id="second-last-name" name="second-last-name" required>
         </div>
+        <br>
         <button type="submit">Registrar</button>
     </form>
 </x-layout>
