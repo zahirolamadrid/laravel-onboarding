23f850f259167407c88b020d49b245b08dc589a7
Diana <dlgarces9218@gmail.com>
2023-12-17 17:22:19 -0600

    Fix all styles problems in the code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
