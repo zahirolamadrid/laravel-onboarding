5ec0a6b6aed72645f74811848ee91914c2290419
Diana <dlgarces9218@gmail.com>
2023-12-18 13:55:21 -0600

    Add style css to add_products view

--- resources/views/leads/add_products.blade.php
+++ resources/views/leads/add_products.blade.php
@@ -1,6 +1,13 @@
 <x-layout>
     <h1>Selecciona lo que necesitas</h1>
     <p>Elige una o varias de las soluciones que tenemos para hacer crecer tu empresa y comenzar a ganar.</p>
+    <style>
+        .marco{
+            border:1px solid black;
+            padding:10px;
+            margin-bottom: 10px;
+        }
+    </style>
     <form action="{{'leads.add_products'}}" method="post">
         @csrf
         <section>
@@ -9,13 +16,11 @@
                    <h2>Plataforma empresarial y Crédito</h2><!-- Título campo -->
                 </legend>
                     @foreach($products as $product)
-                        <div>
+                        <div class="marco">
+                            <input id="product-name" type="checkbox" name="product-name" checked>
+                            <label for="product-name" class="info">{{$product->name}}</label>
                             <br>
-                            <input id="invoicing" type="checkbox" name="invoicing" checked>
-                            <label for="invoicing" class="info">{{$product->name}}</label>
-                            <br>
-                            <label for="invoicing" class="info">{{$product->description}}</label>
-                            <br>
+                            <label for="product-description" class="info">{{$product->description}}</label>
                         </div>
                     @endforeach
             </fieldset>
