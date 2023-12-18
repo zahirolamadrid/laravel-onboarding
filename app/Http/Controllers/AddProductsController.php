fdc785b276a985a853b64d5b7894f7f5f2581656
Diana <dlgarces9218@gmail.com>
2023-12-18 13:17:58 -0600

    Show products obtain from DB

--- app/Http/Controllers/AddProductsController.php
+++ app/Http/Controllers/AddProductsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\DB;
 
 class AddProductsController extends Controller
 {
@@ -14,7 +16,9 @@ class AddProductsController extends Controller
      */
     public function __invoke(Request $request): View|Application|Factory
     {
-        return view('leads.add_products');
+        $products = new Product();
+        $products = DB::table('products')->get();
+        return view('leads.add_products', compact('products'));
     }
 }
 
--- resources/views/leads/add_products.blade.php
+++ resources/views/leads/add_products.blade.php
@@ -5,28 +5,22 @@
         @csrf
         <section>
             <fieldset>
-                <legend><h3>Plataforma empresarial</h3></legend><!-- Título campo -->
-                <div>
-                    <input id="invoicing" type="checkbox" name="invoicing" checked>
-                    <label for="invoicing" class="info">Facturación</label>
-                    <input id="payroll" type="checkbox" name="payroll">
-                    <label for="payroll" class="info">Nómina</label>
-                </div>
+                <legend>
+                   <h2>Plataforma empresarial y Crédito</h2><!-- Título campo -->
+                </legend>
+                    @foreach($products as $product)
+                        <div>
+                            <br>
+                            <input id="invoicing" type="checkbox" name="invoicing" checked>
+                            <label for="invoicing" class="info">{{$product->name}}</label>
+                            <br>
+                            <label for="invoicing" class="info">{{$product->description}}</label>
+                            <br>
+                        </div>
+                    @endforeach
             </fieldset>
-            <fieldset>
-            <legend><h3>Crédito</h3></legend>
                 <br>
-                <div>
-                    <input id="credit-card" type="checkbox" name="credit-card" checked>
-                    <label for="credit-card" class="info">Tarjeta de crédito</label>
-                    <input id="pyme-credit" type="checkbox" name="pyme-credit">
-                    <label for="pyme-credit" class="info">Crédito PyME</label>
-                    <input id="flex-credit" type="checkbox" name="flex-credit">
-                    <label for="flex-credit" class="info">Crédito FLEX</label>
-                </div>
-            </fieldset>
-        <br>
-        <button type="submit">Siguiente</button>
+                <button type="submit">Siguiente</button>
         </section>
     </form>
 </x-layout>
