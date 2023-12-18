5d0548aae3da72378f8a1b6c66d12c04c50ab3c4
raulg <rgnew2023@gmail.com>
2023-12-18 00:08:42 -0500

    Address view

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -1,49 +1,49 @@
 <x-layout>
-    <br>
+        <br>
     <h1>FORMULARIO DE DIRECCION</h1>
     <form method="POST" action="/address">
         @csrf
-    <br>
+        <br>
     <div>
         <label for="street">Calle:</label>
         <input type="text" id="street" name="street" placeholder="Calle" required>
     </div>
-    <br>
+        <br>
     <div>
         <label for="number">Número:</label>
         <input type="text" id="number" name="number" placeholder="Número" required>
     </div>
-    <br>
+        <br>
     <div>
         <label for="inner-number">Número interior:</label>
         <input type="text" id="inner-number" name="inner-number" placeholder="Número interior" required>
     </div>
-    <br>
+        <br>
     <div>
-        <label for="colony">Colonia o Municipalidad:</label>
-        <input type="text" id="colony" name="colony" placeholder="Colonia o Municipalidad" required>
+        <label for="colony">Colonia:</label>
+        <input type="text" id="colony" name="colony" placeholder="Colonia" required>
     </div>
-    <br>
-    <div>
-        <label for="delg">Delegación o Municipalidad:</label>
-        <input type="text" id="delg" name="delg" placeholder="Delegación o Municipalidad" required>
+        <br>
+        <div>
+        <label for="Municipality">Delegación o Municipio:</label>
+        <input type="text" id="Municipality" name="Municipality" placeholder="Delegación o Municipio" required>
     </div>
-    <br>
-    <div>
-        <label for="delg">Delegación o Ciudad:</label>
-        <input type="text" id="delg" name="delg" placeholder="Delegación o Ciudad" required>
-    </div>
-    <br>
+        <br>
     <div>
         <label for="state">Estado:</label>
         <input type="text" id="state" name="state" placeholder="Estado" required>
     </div>
-    <br>
+        <br>
+        <div>
+            <label for="city">Ciudad:</label>
+            <input type="text" id="city" name="state" placeholder="Ciudad" required>
+        </div>
+        <br>
     <div>
         <label for="postcode">Código Postal:</label>
         <input type="text" id="postcode" name="postcode" placeholder="Código Postal" required>
     </div>
-    <br>
+        <br>
     <button type="submit">Registrar</button>
     </form>
     </x-layout>
