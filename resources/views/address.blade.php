d79ee82929fc5c8e45e2a53fd9eecea84f967ce3
raulg <rgnew2023@gmail.com>
2023-12-18 19:01:20 -0500

    Address view

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -36,9 +36,41 @@
         <br>
     <div>
         <label for="state">Estado:</label>
-        <input type="text" id="state" name="state" placeholder="Estado" required>
-    </div>
+        <select name="state" id="state">
+            <option value="estadomexico">Estado de México</option>
+            <option value="jalisco">Jalisco</option>
+            <option value="michoacan">Michoacán</option>
+            <option value="ciudadmexico">Ciudad de México</option>
+            <option value="yucatan">Yucatán</option>
+            <option value="bcalifornia">Baja California</option>
+            <option value="chiapas">Chiapas</option>
+            <option value="nleon">Nuevo León</option>
+            <option value="guanajuato">Guanajuato</option>
+            <option value="hidalgo">Hidalgo</option>
+            <option value="sonora">Sonora</option>
+            <option value="tamaulipas">Tamaulipas</option>
+            <option value="nayarit">Nayarit</option>
+            <option value="tlaxcala">Tlaxcala</option>
+            <option value="qroo">Quintana Roo</option>
+            <option value="cohauila">Cohauila de Zaragoza</option>
+            <option value="morelos">morelos</option>
+            <option value="slpotosi">San Luis Potosí</option>
+            <option value="durango">Durango</option>
+            <option value="tabasco">Tabasco</option>
+            <option value="chihuahua">Chihuahua</option>
+            <option value="sinaloa">Sinaloa</option>
+            <option value="bcaliforniasur">Baja California Sur</option>
+            <option value="veracruz">Veracruz</option>
+            <option value="puebla">Puebla</option>
+            <option value="queretano">Querétano</option>
+            <option value="oaxaca">Oaxaca</option>
+            <option value="zacateca">Zacatecas</option>
+            <option value="acaliente">Aguascalientes</option>
+            <option value="colima">Colima</option>
+            <option value="campeche">Campeche</option>
 
+        </select>
+    </div>
         <br>
     <div>
         <label for="postcode">Código Postal:</label>
