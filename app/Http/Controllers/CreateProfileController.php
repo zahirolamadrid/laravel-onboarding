68b9a3fc90a11b5bcdaca0725de63234d5e2cf2f
Johanna <johannagarces1073@gmail.com>
2023-12-25 16:30:59 -0500

    Controller for new lead

--- app/Http/Controllers/CreateProfileController.php
+++ app/Http/Controllers/CreateProfileController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
 
 class CreateProfileController extends Controller
 {
     public function show(): View
     {
-        return view('create-profile');
+        return view('/create-profile');
+    }
+
+
+    public function store(Request $request): RedirectResponse
+    {
+        $profile = new Lead();
+        $profile->id = $request->id;
+        $profile->name = $request->name;
+        $profile->second_name = $request->second_name;
+        $profile->lastname = $request->lastname;
+        $profile->second_lastname = $request->second_lastname;
+        $profile->email = $request->email;
+        $profile->password = $request->password;
+        $profile->phone = $request->phone;
+        $profile->save();
+            return redirect()->route('create-profile.show')
+        ->withSuccess('Se ha registrado satisfactoriamente.');
+
     }
 }
