84a3d12702b78c15656e58aa2b2afa88ca23ec9a
Johanna <johannagarces1073@gmail.com>
2023-12-26 16:48:00 -0500

    Dont eliminate the privacy checkbox, just the attribute

--- resources/views/create-profile.blade.php
+++ resources/views/create-profile.blade.php
@@ -53,6 +53,11 @@
             </fieldset>
         </section>
         <br>
+        <div>
+            <input id="privacy" type="checkbox" name="privacy">
+            <label for="privacy" class="terms">He leído y acepto el <a href=""><strong>Aviso de Privacidad</strong></a></label>
+        </div>
+        <br>
         <button type="submit">Siguiente</button>
     </form>
 </x-layout>
