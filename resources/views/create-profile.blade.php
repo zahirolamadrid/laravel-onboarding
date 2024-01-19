2435c76729a628369905b5d7225da2d6279bc84b
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-01-19 11:17:19 -0600

    Merge from main

--- resources/views/create-profile.blade.php
+++ resources/views/create-profile.blade.php
@@ -1,5 +1,19 @@
-<x-layout>
-    <h1>Crea tu perfil</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--<h1>Crea tu perfil</h1>
     <hr>
     <p>Escribe los siguientes datos
         <br>
@@ -59,5 +73,5 @@
         </div>
         <br>
         <button type="submit">Siguiente</button>
-    </form>
-</x-layout>
+    </form>--}}
+</x-app-layout>
